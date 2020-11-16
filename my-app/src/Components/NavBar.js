76dfdccd38ce30ca6fcede85120ae5721b241d73
16519111 <gregdimasbaskara@gmail.com>
2020-11-16 09:13:11 +0700

    Revisi NavBar

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../Styles/bootstrap.min.css"
 function NavBar(){
 
-    const [showNav, setShowNav] = React.useState(true)
+    const [showNav, setShowNav] = React.useState(false)
 
     function toggleNav() {
         setShowNav(!showNav)
