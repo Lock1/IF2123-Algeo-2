06ed680322f1ee206c253d50587651df44ecdeb1
16519111 <gregdimasbaskara@gmail.com>
2021-02-19 08:11:47 +0700

    Finalisasi

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -10,30 +10,32 @@ function NavBar(){
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div>
-                    <a className="navbar-brand" href="/">JONG JAVA</a>
-                </div>
-                <button className="navbar-toggler collapsed" type="button" onClick={toggleNav}>
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className={(showNav ? 'show' : '') + ' collapse navbar-collapse'}>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/About">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Concept">App Concept</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/How-to-Use">How To Use</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Search-Engine">Search Engine</a>
-                        </li>                    
-                    </ul>
+                <div className="container">
+                    <div>
+                        <a className="navbar-brand" href="/">JONG JAVA</a>
+                    </div>
+                    <button className="navbar-toggler collapsed" type="button" onClick={toggleNav}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={(showNav ? 'show' : '') + ' collapse navbar-collapse'}>
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/About">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Concept">App Concept</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/How-to-Use">How To Use</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Search-Engine">Search Engine</a>
+                            </li>                    
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
